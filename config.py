19776422170d345a2dd6782cff62adee4b76c4fe
saeed7848 <qs.707@hotmail.com>
2025-09-19 07:05:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# التوكن حق البوت من BotFather
+TELEGRAM_TOKEN = "8230094281:AAEzAJ9GsK5kGqAxYBtH4UdJc2nDQ7kk4lQ"
+
+# ID الشات (القروب أو حسابك) عشان يوصلك التنبيه
+CHAT_ID = "-1003055214239"
+
+# رابط API للأخبار (مثلاً NewsAPI أو أي مصدر تختاره)
+NEWS_API = "https://newsapi.org/v2/everything?q=stocks&apiKey=277a6d15a59b40d6ac43db37ac7627fa"
